06d03152d02fa21bc80f8bf7850d4c2376c376b5
ChevyNo1 <i.collect.70s.stuff@gmail.com>
2010-04-07 22:29:43 -0700

    first commit

---
+++ SimplyStunning.js
@@ -0,0 +1,10 @@
+{
+        developer: "ChevyNo1 Simply Stunning",
+        id: "chevyno1",
+	summary: "Kernels and ROMS for Motorola Droid",
+        manifest: "http://www.clockworkmod.com/ROMManagerManifest/chevyno1.js",
+        roms:
+        {
+            sholes: true
+        },
+}
